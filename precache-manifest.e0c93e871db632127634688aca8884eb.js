d964ac8b86141525a595caaffcdf7ff4b0dc78a2
Eginoj <Eginoj@users.noreply.github.com>
2020-03-14 19:28:26 -0400

    whatever

---
+++ precache-manifest.e0c93e871db632127634688aca8884eb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d41d93c1751cd4fb78d1898f454381",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d1ebea77180ca64390b",
+    "url": "/static/css/2.ac5cd1e0.chunk.css"
+  },
+  {
+    "revision": "6bac9648289cc914dcbe",
+    "url": "/static/css/main.a7627449.chunk.css"
+  },
+  {
+    "revision": "3d1ebea77180ca64390b",
+    "url": "/static/js/2.7a19e61f.chunk.js"
+  },
+  {
+    "revision": "a566d62ae7f18ed31f6ffed31292ac9b",
+    "url": "/static/js/2.7a19e61f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bac9648289cc914dcbe",
+    "url": "/static/js/main.402198c6.chunk.js"
+  },
+  {
+    "revision": "0657bd1b5eb9d49c9743",
+    "url": "/static/js/runtime-main.0fc3ea87.js"
+  },
+  {
+    "revision": "b1521847b5518aaadc59fb3dc19c3416",
+    "url": "/static/media/EGJres.b1521847.png"
+  },
+  {
+    "revision": "15cb7faa02437c2f9719351c157fe7e7",
+    "url": "/static/media/font-awesome.15cb7faa.less"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
